24dcca5a5c6b6cbce5b9295af139ca50738b0020
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-10-08 19:33:40 -0300

    Update Quantidadeusuarios.js

--- graficos/Quantidadeusuarios.js
+++ graficos/Quantidadeusuarios.js
@@ -1,4 +1,4 @@
-import {getcss } from "./common.js"
+import {getcss, tickConfig } from "./common.js"
      async function QuantidadeusuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
